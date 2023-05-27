5f9cbcc5aaeb86dc1b2f05e31d0edc1f98cfb530
Tamar  khuskivadze <131603309+TKhuskivadze@users.noreply.github.com>
2023-05-27 22:49:37 +0400

    add

---
+++ main.js
@@ -0,0 +1,14 @@
+let navigation_bar = document.querySelector('.navbar')
+let menus_btn = document.querySelector(' .menu-btn')
+let close_btn = document.querySelector(' .close-btn')
+
+
+menus_btn.addEventListener('click' , function()  {
+    navigation_bar.classList.add("active")
+    
+})
+
+
+close_btn.addEventListener('click' , function()  {
+    navigation_bar.classList.remove("active")
+})
